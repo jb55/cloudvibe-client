8aedbd15b3bd56e0fcb2945aa015b88d1fe129e8
Bill Casarin <jb@jb55.com>
2010-11-11 22:14:34 -0500

    chordextractor builds with runtime errors

--- cmake/FindCLAM.cmake
+++ cmake/FindCLAM.cmake
@@ -5,9 +5,39 @@ FIND_PATH(CLAM_INCLUDE_DIR CLAMVersion.hxx
   /opt/local/include/CLAM
 )
 
+LINK_DIRECTORIES(
+  /opt/local/lib
+)
+
+SET(CLAM_SHARED_LIBS clam_core clam_processing clam_audioio)
+
+SET(CLAM_STATIC_LIBS
+  ${CMAKE_SOURCE_DIR}/libs/clam/scons/libs/core/libclam_core.a
+  ${CMAKE_SOURCE_DIR}/libs/clam/scons/libs/processing/libclam_processing.a
+  ${CMAKE_SOURCE_DIR}/libs/clam/scons/libs/audioio/libclam_audioio.a
+) 
+
+#SET(CLAM_LIBS clam_core clam_processing clam_audioio)
+
+SET(CLAM_LIB_DEPS 
+  mad
+  ogg
+  FLAC
+  vorbis
+  sndfile
+  vorbisenc
+  vorbisfile
+  id3
+  fftw3
+  pthread
+)
+
+# Use static libs
+SET(CLAM_LIBS ${CLAM_STATIC_LIBS} ${CLAM_LIB_DEPS})
+
 FIND_LIBRARY(CLAM_LIBRARY_DIR
   NAMES 
-    clam_processing clam_audioio clam_core
+    ${CLAM_LIBS}
   PATH
     /usr/local/lib
     /usr/lib
