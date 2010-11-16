1a7978533190b14be5f81316d72eb13d8d7c047a
Bill Casarin <jb@jb55.com>
2010-11-16 18:53:50 -0500

    Updated linux build to use shared libs for now

--- cmake/FindCLAM.cmake
+++ cmake/FindCLAM.cmake
@@ -5,6 +5,10 @@ FIND_PATH(CLAM_INCLUDE_DIR CLAMVersion.hxx
   /opt/local/include/CLAM
 )
 
+IF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+  SET(PlatformLinux True)
+ENDIF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+
 LINK_DIRECTORIES(
   /opt/local/lib
 )
@@ -16,6 +20,8 @@ SET(CLAM_STATIC_LIBS
   ${CMAKE_SOURCE_DIR}/libs/clam/scons/libs/processing/libclam_processing.a
   ${CMAKE_SOURCE_DIR}/libs/clam/scons/libs/audioio/libclam_audioio.a
 ) 
+
+ADD_DEFINITIONS(-DUSE_SNDFILE -DUSE_FFTW3 -DUSE_MAD -DUSE_OGGVORBIS)
 
 SET(CLAM_LIB_DEPS 
   mad
@@ -29,8 +35,18 @@ SET(CLAM_LIB_DEPS
   fftw3
 )
 
-# Use static libs
-SET(CLAM_LIBS ${CLAM_STATIC_LIBS} ${CLAM_LIB_DEPS})
+IF (PlatformLinux)
+  SET(CLAM_LIB_DEPS ${CLAM_LIB_DEPS}
+    dl
+  )
+
+  # Used shared libs on linux for now
+  SET(CLAM_LIBS ${CLAM_SHARED_LIBS} ${CLAM_LIB_DEPS})
+ELSE (PlatformLinux)
+
+  # Static libs on osx
+  SET(CLAM_LIBS ${CLAM_STATIC_LIBS} ${CLAM_LIB_DEPS})
+ENDIF (PlatformLinux)
 
 FIND_LIBRARY(CLAM_LIBRARY_DIR
   NAMES 
